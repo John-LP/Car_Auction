363dd7363c229d734d7d69535c502df964a5f3c5
John-LP <jonathanleplat@hotmail.fr>
2023-11-08 15:54:09 +0100

    mise en page form_annonce

--- views/form_annonce.php
+++ views/form_annonce.php
@@ -5,13 +5,22 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
-  <!-- <link href="./css/styles_form.css" rel="stylesheet"> -->
+  <link href="./../css/styles_form.css" rel="stylesheet">
 </head>
 <body>
   <?php
-
   require __DIR__ . "/navbar2.php";
-
   ?>
+  <div id="contactForm">
+    <h1 style="margin-bottom: 10px;">Votre annonce</h1>
+    <form action="class_annonce.php">
+      <input placeholder="Name" type="text" required />
+      <input placeholder="Email" type="email" required />
+      <input placeholder="Subject" type="text" required />
+      <input placeholder="Message"/>
+      <button class="formBtn" type="submit">Submit</button>
+    </form>
+  </div>
+  <script src="/js/main.js"></script>
 </body>
 </html>
