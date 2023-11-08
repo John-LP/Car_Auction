153e090fa57fe1fcbde1b4ee1ccec4204e53d12c
John-LP <jonathanleplat@hotmail.fr>
2023-11-08 15:01:33 +0100

    MàJ page login, tous les names des inputs prêt à l'utilisation

--- views/login.php
+++ views/login.php
@@ -10,7 +10,7 @@
             href="https://unicons.iconscout.com/release/v2.1.9/css/unicons.css">
         <link rel="stylesheet"
             href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css">
-            <link href="./css/styles.css" rel="stylesheet">
+            <link href="./../css/styles.css" rel="stylesheet">
     </head>
     <body>
         <div class="section">
@@ -18,8 +18,7 @@
                 <div class="row full-height justify-content-center">
                     <div class="col-12 text-center align-self-center py-5">
                         <div class="section pb-5 pt-5 pt-sm-2 text-center">
-                            <h6 class="mb-0 pb-3"><span>Log In </span><span>Sign
-                                    Up</span></h6>
+                            <h6 class="mb-0 pb-3"><span>Connexion </span><span>Inscription</span></h6>
                             <input class="checkbox" type="checkbox" id="reg-log"
                                 name="reg-log" />
                             <label for="reg-log"></label>
@@ -28,9 +27,10 @@
                                     <div class="card-front">
                                         <div class="center-wrap">
                                             <div class="section text-center">
-                                                <h4 class="mb-4 pb-3">Log In</h4>
+                                                <h4 class="mb-4 pb-3">Connexion</h4>
                                                 <div class="form-group">
                                                     <input type="email"
+                                                        name="email"
                                                         class="form-style"
                                                         placeholder="Email">
                                                     <i
@@ -38,41 +38,44 @@
                                                 </div>
                                                 <div class="form-group mt-2">
                                                     <input type="password"
+                                                        name="mdp"
                                                         class="form-style"
-                                                        placeholder="Password">
+                                                        placeholder="Mot de passe">
                                                     <i
                                                         class="input-icon uil uil-lock-alt"></i>
                                                 </div>
                                                 <a
                                                     href="https://www.web-leb.com/code"
-                                                    class="btn mt-4">Login</a>
+                                                    class="btn mt-4">Se connecter</a>
                                                 <p class="mb-0 mt-4 text-center"><a
                                                         href="https://www.web-leb.com/code"
-                                                        class="link">Forgot your
-                                                        password?</a></p>
+                                                        class="link">Mot de passe oublié?</a></p>
                                             </div>
                                         </div>
                                     </div>
                                     <div class="card-back">
                                         <div class="center-wrap">
                                             <div class="section text-center">
-                                                <h4 class="mb-3 pb-3">Sign Up</h4>
+                                                <h4 class="mb-3 pb-3">S'inscrire</h4>
                                                 <div class="form-group">
                                                     <input type="text"
+                                                        name="nom"
                                                         class="form-style"
-                                                        placeholder="Full Name">
+                                                        placeholder="Nom">
                                                     <i
                                                         class="input-icon uil uil-user"></i>
                                                 </div>
                                                 <div class="form-group mt-2">
                                                     <input type="tel"
+                                                        name="prenom"
                                                         class="form-style"
-                                                        placeholder="Phone Number">
+                                                        placeholder="Prénom">
                                                     <i
-                                                        class="input-icon uil uil-phone"></i>
+                                                    class="input-icon uil uil-user"></i>
                                                 </div>
                                                 <div class="form-group mt-2">
                                                     <input type="email"
+                                                        name="email"
                                                         class="form-style"
                                                         placeholder="Email">
                                                     <i
@@ -80,14 +83,15 @@
                                                 </div>
                                                 <div class="form-group mt-2">
                                                     <input type="password"
+                                                        name="mdp"
                                                         class="form-style"
-                                                        placeholder="Password">
+                                                        placeholder="Mot de passe">
                                                     <i
                                                         class="input-icon uil uil-lock-alt"></i>
                                                 </div>
                                                 <a
                                                     href="https://www.web-leb.com/code"
-                                                    class="btn mt-4">Register</a>
+                                                    class="btn mt-4">Créer le compte</a>
                                             </div>
                                         </div>
                                     </div>
