e6863f86d8f60be4d33e8fb3ed8c16bfe6523a2a
John-LP <jonathanleplat@hotmail.fr>
2023-11-09 17:10:17 +0100

    finalisation accueil.php

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="./css/styles_navbar.css" rel="stylesheet">
+  <link href="./css/styles_card.css" rel="stylesheet" type="text/css" />
   <title>Car Auction</title>
 </head>
 <body>
--- views/accueil.php
+++ views/accueil.php
@@ -1,40 +1,38 @@
-<?php
-    require_once __DIR__."/../classes/class_annonce.php";
-    ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="style.css" rel="stylesheet"> 
+    <link href="./../css/styles_card.css" rel="stylesheet" type="text/css" />
+    <link href="./../css/styles_navbar.css" rel="stylesheet">
 </head>
-<body>
-
-    <div class="render_annonces">
+    <body>
         <?php
-            $dbh = new PDO("mysql:dbname=car_auction;host=127.0.0.1", "root", "");
-            if (isset($_GET['id'])) {
-                $selectedID = $_GET['id'];
-            $query = $dbh->prepare('SELECT annonces.*,utilisateurs.id_utilisateur from annonces LEFT JOIN utilisateurs ON utilisateurs.id_utilisateur=annonces.id_utilisateur;');
-            $query->bindParam(':selectedID', $selectedID, PDO::PARAM_INT);
-            } else {
-                $query = $dbh->prepare('SELECT annonces.*,utilisateurs.id_utilisateur from annonces LEFT JOIN utilisateurs ON utilisateurs.id_utilisateur=annonces.id_utilisateur');
-            }
-            $query->execute();
-            $results = $query->fetchAll();
-            foreach($results as $key => $values) { 
-            echo "<div class='renderclients'>";
-            echo "<p> Marque: " . $values['marque'] . "</p>" ;
-            echo "<p> Prix de départ: " . $values['prix_depart'] . "</p>" ;
-            echo "<p> Date " . $values['date_fin'] . "</p>" ;
-            echo "<a class='info' href='details_annonce.php?id_annonce=" . $values['id_annonce'] . "'>En savoir plus</a>";
-            echo "</div>";
-            }
+        $dbh = new PDO("mysql:dbname=car_auction;host=localhost", "root", "");
+
+        $query = $dbh->prepare("SELECT annonces.*, utilisateurs.nom, utilisateurs.prenom FROM annonces 
+                      INNER JOIN utilisateurs ON annonces.id_utilisateur = utilisateurs.id_utilisateur");
+        $query->execute();
+
+        while ($result = $query->fetch()) {
+            echo '<article>';
+            echo '<section class="card">';
+            echo '<div class="text-content">';
+            echo "<h3>Par " . $result['nom'] . ' ' . $result['prenom'] . "</h3>";
+            echo '<br><br>';
+            echo "<h3>" . $result['marque'] . " " . $result['modele'] . "</h3>";
+            echo "<p><u>Année :</u>" . " " . $result['annee'] . "</p>";
+            echo "<p><u>Prix de départ :</u> " . $result['prix_depart'] . "€" . "</p>";
+            echo "<p><u>Échéance de l'enchère :</u> " . $result['date_fin'] . "</p>";
+            echo "<a class='info'>Enchérir</a>";
+            echo "<a class='info' href='./views/details_annonce.php?id_annonce=" . $result['id_annonce'] . "'>En savoir plus</a>";
+            echo '</div>';
+            echo '<div class="visual">';
+            echo '<img src="https://api.renault-retail-group.fr/media/cache/csu_thumb_mobile/cdn/photos_rrgvo_hd/7/3137791_izajq.webp" alt />';
+            echo '</div>';
+            echo '</section>';
+            echo '</article>';
+        }
         ?>
-    </div>
-
-
-
-</body>
+    </body>
 </html>
