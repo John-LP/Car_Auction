6744e44b84b4ae4de395de3d7060287c96b1ca4f
floriane6027 <lunettafloriane24@gmail.com>
2023-11-09 16:34:35 +0100

    crea class_ utilisateur, timeout,redirection etc...

--- classes/class_utilisateur.php
+++ classes/class_utilisateur.php
@@ -1,4 +1,14 @@
-<!-- Création d'un compte utilisateur -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./../css/styles_login.css">
+    <title>Document</title>
+</head>
+<body>
+    
+
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nom = $_POST['nom'];
@@ -6,7 +16,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST['email'];
     $mdp = $_POST['mdp'];
 
-    $dbh = new PDO("mysql:dbname=car_auction;host=localhost", "root", "");
+    $dbh = new PDO("mysql:dbname=car_auction;localhost=8889", "root", "root");
 
     $query = $dbh->prepare("INSERT INTO utilisateurs (nom, prenom, email, mdp) VALUES (?, ?, ?, ?)");
     $res = $query->execute([$nom, $prenom, $email, $mdp]);
@@ -16,14 +26,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $query->execute([$nom]);
         $utilisateur = $query->fetch();
 
-        echo "<p>Nom: " . $utilisateur['nom'] . "</p>";
-        echo "<p>Prénom: " . $utilisateur['prenom'] . "</p>";
-        echo "<p>Mail: " . $utilisateur['email'] . "</p>";
+        echo "<p>Votre compte a bien été créé. <br>
+                Vous allez être redirigé à l'accueil.<br>
+                Bonne visite.</p>";
 
     } else {
-
-        echo "Erreur lors de l'insertion dans la base de données.
-        Veuillez remplir tous les champs.";
+        echo "<p>Erreur lors de l'insertion dans la base de données. Veuillez remplir tous les champs.</p>";
     }
+
+    echo '<script>
+            setTimeout(function() {
+                window.location.href = "http://localhost:8888/php/car_auction/index.php";
+            }, 1000); // Redirection après 1 secondes (1000 millisecondes)
+          </script>';
 }
 ?>
+</body>
+</html>
