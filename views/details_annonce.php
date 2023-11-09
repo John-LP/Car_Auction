d4abb81b66d5d02a74b748d2822a9ddf7aee518f
John-LP <jonathanleplat@hotmail.fr>
2023-11-09 16:06:59 +0100

    details_annonce

--- views/details_annonce.php
+++ views/details_annonce.php
@@ -8,29 +8,32 @@
         <link href="./../css/styles_card.css" rel="stylesheet" type="text/css" />
         <link href="./../css/styles_navbar.css" rel="stylesheet">
     </head>
-
     <body>
     <?php
     require_once __DIR__ . "/navbar.php";
+
     $dbh = new PDO("mysql:dbname=car_auction;host=localhost", "root", "");
 
     $query = $dbh->prepare("SELECT * FROM annonces");
     $query->execute();
 
     while ($result = $query->fetch()) {
-      echo '<article>';
-      echo '<section class="card">';
-          echo '<div class="text-content">';
-            echo "<h3>" . $result['marque'] . " " . $result['modele'] . " année " . $result['annee'] . "</h3>";
-            echo "<p><u>Prix de départ :</u> " . $result['prix_depart'] . "€" . "</p>";
-            echo "<p><u>Échéance de l'enchère :</u> " . $result['date_fin'] . "</p>";
-            echo "<p><u>Puissance :</u> " . $result['puissance'] . " Ch" . "</p>";
-            echo "<p>" . $result['description'] . "</p>";
-            echo '<a href="http://localhost/exoPHP/car_auction/">Retour</a>';
-            echo '</div>';
-            echo '<div class="visual">';
-            echo '<img src="https://api.renault-retail-group.fr/media/cache/csu_thumb_mobile/cdn/photos_rrgvo_hd/7/3137791_izajq.webp" alt />';
+        echo '<article>';
+        echo '<section class="card">';
+        echo '<div class="text-content">';
+        echo "<h3>" . $result['marque'] . " " . $result['modele'] . " année " . $result['annee'] . "</h3>";
+        echo "<p><u>Prix de départ :</u> " . $result['prix_depart'] . "€" . "</p>";
+        echo "<p><u>Échéance de l'enchère :</u> " . $result['date_fin'] . "</p>";
+        echo "<p><u>Puissance :</u> " . $result['puissance'] . " Ch" . "</p>";
+        echo "<p>" . $result['description'] . "</p>";
+        echo '<a href="http://localhost/exoPHP/car_auction/">Retour</a>';
+        echo '</div>';
+        echo '<div class="visual">';
+        echo '<img src="https://api.renault-retail-group.fr/media/cache/csu_thumb_mobile/cdn/photos_rrgvo_hd/7/3137791_izajq.webp" alt />';
+        echo '</div>';
+        echo '</section>';
+        echo '</article>';
     }
-?>
+    ?>
     </body>
 </html>
