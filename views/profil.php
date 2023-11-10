85c72a2a4d497a6aedddda7a70e33e55b9139bee
floriane6027 <lunettafloriane24@gmail.com>
2023-11-10 10:29:49 +0100

    profil pour afficher les data recuperees ds class_utilisateurs

---
+++ views/profil.php
@@ -0,0 +1,36 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="./../css/styles_card.css" rel="stylesheet" type="text/css" />
+    <link href="./../css/styles_navbar.css" rel="stylesheet">
+</head>
+    <body>
+        <?php
+        $dbh = new PDO("mysql:dbname=car_auction;host=localhost;port=8889", "root", "root");
+
+        $query = $dbh->prepare("SELECT * FROM utilisateurs");
+        $query->execute();
+
+        while ($result = $query->fetch()) {
+            echo '<article>';
+            echo '<section class="card">';
+            echo '<div class="text-content">';
+            echo "<p><u>Votre ID unique à utiliser pour créer une annoce :</u> " . $result['id_utilisateur'] . "</p>";
+            echo '<br>';
+            echo "<p><u>Nom :</u> " . $result['nom'] . "</p>";
+            echo '<br>';
+            echo "<p><u>Prénom :</u> " . $result['prenom'] . "</p>";
+            echo '<br>';
+            echo "<p><u>Email :</u> " . $result['email'] . "</p>";
+            echo "<br>";
+            echo "<a class='info'>Modifier</a>";
+            echo '<br><br>';
+            echo '</section>';
+            echo '</article>';
+        }
+        ?>
+    </body>
+</html>
