5fae4222d89dd8df119dce61135937e562a3c8d4
John-LP <jonathanleplat@hotmail.fr>
2023-11-10 16:24:19 +0100

    responsive formulaire, fin du css card

--- views/form_annonce.php
+++ views/form_annonce.php
@@ -18,12 +18,12 @@
   <div id="contactForm">
     <h1 style="margin-bottom: 10px;">Votre annonce</h1>
     <form action="./../classes/class_annonce.php" method="POST" enctype="multipart/form-data">
+      <label>Échéance de l'enchère :</label>
+      <input type="date" name="date_fin" min="<?= date('Y-m-d') ?>" required />
       <input placeholder="Votre ID (visible depuis l'onglet 'Mon Profil')" type="number" name="id_utilisateur" required />
       <input placeholder="Marque du véhicule" type="text" name="marque" required />
       <input placeholder="Modèle" type="text" name="modele" required />
       <input placeholder="Prix de départ" type="number" name="prix_depart" required min="0" />
-      <label>Échéance de l'enchère :</label>
-      <input placeholder="Date de fin" type="date" name="date_fin" min="<?= date('Y-m-d') ?>" required />
       <input placeholder="Puissance" type="number" name="puissance" required min="0" />
       <input placeholder="Année" type="number" id="yearInput" name="annee" min="1985" step="1" required>
       <input placeholder="Description" type="textearea" name="description" required />
