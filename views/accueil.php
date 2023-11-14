d820d40fe764e61eb094e147604c3fd98f4a4589
John-LP <jonathanleplat@hotmail.fr>
2023-11-14 12:04:28 +0100

    ajout d'un form pour filtrer sur accueil.php -> toutes annonces, celles en cours et celles terminées

--- views/accueil.php
+++ views/accueil.php
@@ -10,68 +10,62 @@
 </head>
 
 <body>
-    <h2>Enchères en cours</h2>
+    <div class="filter-section">
+        <form method="post">
+            <label for="filter"></label>
+            <input class='check' type="checkbox" id="filter" name="filter" value="toutes" > Toutes
+            <input class='check' type="checkbox" id="filter" name="filter" value="en_cours" > En cours
+            <input class='check' type="checkbox" id="filter" name="filter" value="terminees"> Terminées
+            <input class='btn_filter' type="submit" value="Appliquer">
+        </form>
+    </div>
     <?php
     // Inclusion de la classe serveur pour la gestion de la base de données
     require_once __DIR__ . "./../classes/class_serveur.php";
-
-    // Requête pour récupérer toutes les annonces avec les détails des utilisateurs associés
-    $query = $dbh->prepare("SELECT annonces.*, utilisateurs.nom, utilisateurs.prenom FROM annonces 
-                      INNER JOIN utilisateurs ON annonces.id_utilisateur = utilisateurs.id_utilisateur WHERE annonces.date_fin > NOW()");
-    $query->execute();
-
-    // Affichage des annonces récupérées
-    while ($result = $query->fetch()) {
-        echo '<article>';
-        echo '<section class="card">';
-        echo '<div class="text-content">';
-        echo "<h3>Mise aux enchères par</n> " . $result['nom'] . ' ' . $result['prenom'] . "</h3>";
-        echo '<br><br>';
-        echo "<h3>" . $result['marque'] . " " . $result['modele'] . "</h3>";
-        echo "<p><u>Année :</u>" . " " . $result['annee'] . "</p>";
-        echo "<p><u>Prix de départ :</u> " . $result['prix_depart'] . " €" . "</p>";
-        echo "<p><u>Échéance de l'enchère :</u> " . $result['date_fin'] . " (exclu)" . "</p>";
-        echo "<a class='info' href='./views/details_annonce.php?id_annonce=" . $result['id_annonce'] . "'>En savoir plus</a>";
-        echo '</div>';
-        echo '<div >';
-        echo "<img class='visual' src='" . $result['image_path'] . "' alt />";
-        echo '</div>';
-        echo '</section>';
-        echo '</article>';
+    $filter_value = '';
+    if (isset($_POST['filter'])) {
+        $filter_value = $_POST['filter'];
     }
-    ?>
+     // Requête pour récupérer toutes les annonces avec les détails des utilisateurs associés
+     $query = $dbh->prepare("SELECT annonces.*, utilisateurs.nom, utilisateurs.prenom FROM annonces 
+                     INNER JOIN utilisateurs ON annonces.id_utilisateur = utilisateurs.id_utilisateur");
+     $query->execute();
 
-    <h2>Enchères terminées</h2>
-    <?php
-    // Inclusion de la classe serveur pour la gestion de la base de données
-    require_once __DIR__ . "./../classes/class_serveur.php";
+        if ($filter_value == 'en_cours') {
+        // Requête pour récupérer toutes les annonces avec les détails des utilisateurs associés
+        $query = $dbh->prepare("SELECT annonces.*, utilisateurs.nom, utilisateurs.prenom FROM annonces 
+                        INNER JOIN utilisateurs ON annonces.id_utilisateur = utilisateurs.id_utilisateur WHERE annonces.date_fin > NOW()");
+        $query->execute();
+        echo "<h2><u>Enchères en cours</u></h2>";
 
-    // Requête pour récupérer toutes les annonces avec les détails des utilisateurs associés
-    $end_of_query = $dbh->prepare("SELECT annonces.*, utilisateurs.nom, utilisateurs.prenom FROM annonces 
-                      INNER JOIN utilisateurs ON annonces.id_utilisateur = utilisateurs.id_utilisateur WHERE annonces.date_fin < NOW()");
-    $end_of_query->execute();
-
-    // Affichage des annonces récupérées
-    while ($end_of_result = $end_of_query->fetch()) {
-        echo '<article>';
-        echo '<section class="card">';
-        echo '<div class="text-content">';
-        echo "<h3>Mise aux enchères par</n> " . $end_of_result['nom'] . ' ' . $end_of_result['prenom'] . "</h3>";
-        echo '<br><br>';
-        echo "<h3>" . $end_of_result['marque'] . " " . $end_of_result['modele'] . "</h3>";
-        echo "<p><u>Année :</u>" . " " . $end_of_result['annee'] . "</p>";
-        echo "<p><u>Prix de départ :</u> " . $end_of_result['prix_depart'] . " €" . "</p>";
-        echo "<p><u>Échéance de l'enchère :</u> " . $end_of_result['date_fin'] . "</p>";
-        echo "<a class='info' href='./views/details_annonce.php?id_annonce=" . $end_of_result['id_annonce'] . "'>En savoir plus</a>";
-        echo '</div>';
-        echo '<div >';
-        echo "<img class='visual' src='" . $end_of_result['image_path'] . "' alt />";
-        echo '</div>';
-        echo '</section>';
-        echo '</article>';
-    }
-    ?>
-
+        } elseif ($filter_value == 'terminees') {
+            // Requête pour récupérer toutes les annonces avec les détails des utilisateurs associés
+            $query = $dbh->prepare("SELECT annonces.*, utilisateurs.nom, utilisateurs.prenom FROM annonces 
+                            INNER JOIN utilisateurs ON annonces.id_utilisateur = utilisateurs.id_utilisateur WHERE annonces.date_fin < NOW()");
+            $query->execute();
+            echo "<h2><u>Enchères terminées</u></h2>";
+            } else {
+                echo "<h2><u>Toutes les enchères</u></h2>";
+            }
+// Affichage des annonces récupérées
+while ($result = $query->fetch()) {
+    echo '<article>';
+    echo '<section class="card">';
+    echo '<div class="text-content">';
+    echo "<h3>Mise aux enchères par</n> " . $result['nom'] . ' ' . $result['prenom'] . "</h3>";
+    echo '<br><br>';
+    echo "<h3>" . $result['marque'] . " " . $result['modele'] . "</h3>";
+    echo "<p><u>Année :</u>" . " " . $result['annee'] . "</p>";
+    echo "<p><u>Prix de départ :</u> " . $result['prix_depart'] . " €" . "</p>";
+    echo "<p><u>Échéance de l'enchère :</u> " . $result['date_fin'] . "</p>";
+    echo "<a class='info' href='./views/details_annonce.php?id_annonce=" . $result['id_annonce'] . "'>En savoir plus</a>";
+    echo '</div>';
+    echo '<div >';
+    echo "<img class='visual' src='" . $result['image_path'] . "' alt />";
+    echo '</div>';
+    echo '</section>';
+    echo '</article>';
+}
+?>
 </body>
-
 </html>
